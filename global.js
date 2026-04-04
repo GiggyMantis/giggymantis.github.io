503433953f75dfd06b75eb17e7886044017decd6
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-04-03 22:23:58 -0400

    ...

--- global.js
+++ global.js
@@ -3,8 +3,3 @@ fetch('header.html')
     .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
 });
-fetch('Yukatati/header.html')
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById('yukatati-header-placeholder').innerHTML = data;
-});
