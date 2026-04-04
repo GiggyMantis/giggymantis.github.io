10cfacfd40ee3c485bce8cad8586c4dc97250360
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-04-03 22:19:52 -0400

    Yukatati?

--- global.js
+++ global.js
@@ -3,3 +3,8 @@ fetch('header.html')
     .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
 });
+fetch('Yukatati/header.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('yukatati-header-placeholder').innerHTML = data;
+});
