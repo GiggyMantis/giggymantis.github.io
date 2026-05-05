c2c743bef38632165d6771ef54f2ceda3d711538
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-04 22:39:47 -0400

    testimn

--- Dogha/viewer.js
+++ Dogha/viewer.js
@@ -1,7 +1,9 @@
 let htmlString = "<h1>Voslhemow Interlinear Text Viewer</h1>"
-let verses = Papa.parse(fetch("Voslhemow_Resources/verses.csv"), {
-    header: true
-});
+// let verses = Papa.parse(fetch("Voslhemow_Resources/verses.csv"), {
+//     header: true
+// });
+
+let verses = Papa.parse("test,1,2,3\n1,2,3,4")
 
 console.log(verses.data)
 $("#content").html($.parseHTML(htmlString))
