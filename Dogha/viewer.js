c67ee29111b1cd7935f36fdb822c26077a03be61
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-05 00:42:33 -0400

    i want to go to bed

--- Dogha/viewer.js
+++ Dogha/viewer.js
@@ -14,14 +14,13 @@ async function loadFileAsCSV(url) {
 
 function formatVerse(verse, glossingTerms, concordance) {
     // TODO: make this lol
-    return "<li>" + String(verse) + "</li>"
+    return "<li>" + verse + "</li>"
 }
 
 let htmlString = "<h1>Voslhemow Interlinear Text Viewer</h1>"
 const chapters = loadFileAsCSV("Voslhemow_Resources/chapters.csv").then(result => {
     result.data.forEach(element => {
         const num = element["Number"]
-        // htmlString += "<hgroup><h2 id=\"chapter-" + element["Number"] + "\">Chapter " + element["Number"] + ": " + element["Original"] + "</h2><p class=\"translated-chapter\">Plain Translation: " + element["Plain"] + "<br>EME Translation: " + element["EME"] + "<br>Literal Translation: " + element["Literal"] + "</p></hgroup><ol id=\"list-chapter-" + element["Number"] + "\"></ol>"
         htmlString += `<hgroup><h2 id="chapter-${num}">Chapter ${num}: ${element["Original"]}</h2><p class="translated-chapter">Plain Translation: ${element["Plain"]}<br>EME Translation: ${element["EME"]}<br>Literal Translation: ${element["Literal"]}</p></hgroup><ol id="list-chapter-${num}"></ol>`
     })
 })
@@ -37,7 +36,9 @@ $(document).ready(function(){
         concordance.then(concordance_result => {
             verses.then(verses_result => {
                 verses_result.data.forEach((element, index) => {
-                    $(`#list-chapter-${index}`).append($.parseHTML(formatVerse(element, glossing_result.data, concordance_result.data)))
+                    const verhtml = formatVerse(element, glossing_result.data, concordance_result.data)
+                    console.log(verhtml)
+                    $(`#list-chapter-${index}`).append($.parseHTML(verhtml))
                 })
             })
         })
