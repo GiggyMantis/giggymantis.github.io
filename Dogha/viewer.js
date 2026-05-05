1cf8b09134fed9c6d9af5c6368ded4f9204f1596
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-04 22:11:53 -0400

    debug

--- Dogha/viewer.js
+++ Dogha/viewer.js
@@ -1,1 +1,11 @@
-$("#header-placeholder").html("<p>test</p>")
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+    $("#content").html(data)
+});
+
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+    console.log(data)
+});
