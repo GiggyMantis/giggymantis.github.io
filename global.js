73ecbaa43b408357b093988e6b3befa40e19251a
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-04 20:29:05 -0400

    fixed dogha header and disallowed AI scraping

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ fetch('header.html')
     .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
 });
+
+fetch('Dogha/header.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('dogha-header-placeholder').innerHTML = data;
+});
