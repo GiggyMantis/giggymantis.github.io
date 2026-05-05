40c5ba123004a6eb5466b28a09fc5b67182fb7a1
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-05 12:46:58 -0400

    adad

--- Dogha/concordance.js
+++ Dogha/concordance.js
@@ -18,6 +18,7 @@ const verses = loadFileAsCSV("Voslhemow_Resources/verses.csv")
 $(document).ready(function(){
     verses.then(result => {
         const allHeaders = [...$(":header")]
+        console.log(allHeaders)
         allHeaders.forEach(element => {
             const id = element.attr("id")
             const check = `[${id}]`
