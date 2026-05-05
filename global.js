71899a021f032c74d5324a2d97940634a424f4e8
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-04 20:35:13 -0400

    another attempt

--- global.js
+++ global.js
@@ -3,9 +3,3 @@ fetch('header.html')
     .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
 });
-
-fetch('Dogha/header.html')
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById('dogha-header-placeholder').innerHTML = data;
-});
