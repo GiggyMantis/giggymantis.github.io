515b6fded98525ca07876d893ab40319c611a377
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-04 22:26:56 -0400

    switched to papa.parse

--- Dogha/viewer.js
+++ Dogha/viewer.js
@@ -1,5 +1,5 @@
 let htmlString = "<h1>Voslhemow Interlinear Text Viewer</h1>"
-let verses = $.csv.toObjects(fetch("Voslhemow_Resources/verses.csv").then(response => response.text()))
+let verses = Papa.parse(fetch("Voslhemow_Resources/verses.csv").then(response => response.text()))
 
 console.log(verses)
 $("#content").html($.parseHTML(htmlString))
