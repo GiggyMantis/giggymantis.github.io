9b8272524ca58f582f223215c0a1ecb1b955e3bf
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-06 11:35:05 -0400

    forgot to update that

--- Dogha/concordance.js
+++ Dogha/concordance.js
@@ -15,7 +15,7 @@ async function loadFileAsCSV(url) {
 function formatVerse(verse, glossingTerms, highlightTerm) {
     let words = verse["Original"].split(' ')
     verse["Lemmatized"].split(' ').forEach((lemma, index) => {
-        if (lemma.trim() == `[${highlightTerm.trim()}]`) {
+        if (lemma.trim() == `${highlightTerm.trim()}`) {
             words[index] = `<b><u>${words[index]}</u></b>`
         }
     })
