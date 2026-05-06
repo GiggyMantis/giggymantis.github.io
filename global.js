a42451e22ee397d507bbe7da38f9186cbee6ec76
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-06 11:29:56 -0400

    few fixes

--- global.js
+++ global.js
@@ -1,5 +1,7 @@
-fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-    $("#header-placeholder").html(data)
-});
+$(document).ready(function(){
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+        $("#header-placeholder").html(data)
+    })
+})
