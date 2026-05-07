e0fa65f59bb43a47560e2452b9cf01c09d33030b
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-05-07 11:28:41 -0400

    test

--- Dogha/viewer.js
+++ Dogha/viewer.js
@@ -57,6 +57,7 @@ $(document).ready(function(){
     glossingTerms.then(glossing_result => {
         verses.then(verses_result => {
             verses_result.data.forEach(element => {
+                console.log("loaded verse")
                 const verhtml = formatVerse(element, glossing_result.data)
                 $(`#list-chapter-${element["Chapter"]}`).append($.parseHTML(verhtml))
             })
