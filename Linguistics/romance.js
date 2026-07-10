3a6cecb48aec6b26dc20f30e75bbb9b01189604b
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-10 13:25:30 -0400

    deb

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -82,6 +82,7 @@ function latinate_stress(input) {
         if (new RegExp("[^aeoiuy]\\.(?!.*\\.)").test(input)) {
             // Penult is heavy, stress the penult
             ret = ("." + input).replace(new RegExp("(\\.)(?!.*\\..*\\.)"), "ˈ$1");
+            console.log("heavy penult");
         } else {
             // Penult is liteweit, stress the antepenult
             ret = ("." + input).replace(new RegExp("(\\.)(?!.*\\..*\\..*\\.)"), "ˈ$1");
