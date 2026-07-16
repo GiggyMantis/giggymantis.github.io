cc62a5719c65ba6bbcb29c4f6885cddff36dda9c
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-16 00:35:02 -0400

    ((1 + 2i) * hot) fix

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -123,8 +123,8 @@ const pr_firstpass = {
     "u̯\\.(?=[aeoiuɛɔɪʊ])" : ".w", // semivocalization in unstressed hiatus (and change of notation of /au̯/)
     "u̯" : "w",
     "w" : "W", // temporary notation shift to avoid deletion of preexisting w in geminate locations
-    "(?<!ˈ[^aeoiuɛɔɪʊ]{0,2})[eɛiɪ]\\.(?=[aeoiuɛɔɪʊ])" : ".j",
-    "(?<!ˈ[^aeoiuɛɔɪʊ]{0,2})[oɔuʊ]\\.(?=[aeoiuɛɔɪʊ])" : ".w",
+    "(?<!ˈ[^aeoiuɛɔɪʊ]{0,2})[eɛiɪ]\\.(?=ˈ?[aeoiuɛɔɪʊ])" : ".j",
+    "(?<!ˈ[^aeoiuɛɔɪʊ]{0,2})[oɔuʊ]\\.(?=ˈ?[aeoiuɛɔɪʊ])" : ".w",
     "(?<=ˈ[^aeoiuɛɔɪʊ]{0,2})[eɛiɪ]\\.(?=[aeoiuɛɔɪʊ].*\\.)" : "j", // semivocalization in antepenultimate stressed hiatus with front vowels
     "ˈ([^aeoiuɛɔɪʊ]{0,2})[oɔuʊ]\\.(?=[aeoiuɛɔɪʊ].*\\.)" : "ϝ$1w", // semivocalization in antepenultimate stressed hiatus with back vowels (here using ϝ to mark that the stress needs to be moved one syllable back)
     "\\.([^\\.ϝ]*)\\.ϝ" : ".ˈ$1.",
