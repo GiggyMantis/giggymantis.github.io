c624f01d1c261cde45043abb6c5c1155b77bc88f
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-18 17:23:50 -0400

    what??

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -143,6 +143,7 @@ const pr_firstpass = {
     "(?<=[aeoiuɛɔɪʊ])\\.([^aeoiuɛɔɪʊ])([^aeoiuɛɔɪʊ\\.])" : "$1.$2",
     "([^aeoiuɛɔɪʊ])\\.(ˈ?)\\1w" : "$1.$2$1", // w deletion after geminates
     "W" : "w", // undoes earlier temporary notation shift,
+    "\\.\\." : ".",
     "\\.\\." : ".", 
     // "^([^aeoiuɛɔɪʊ]+)\\.([^aeoiuɛɔɪʊ\\.]+)" : "$1$2.", // misaligned consonants at the start of words fixed
     // "^([^aeoiuɛɔɪʊ]+)ˈ([^aeoiuɛɔɪʊ\\.ˈ]+)" : "ˈ$1$2",
