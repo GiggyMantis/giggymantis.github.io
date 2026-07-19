3171653751fec8ad5697f01fb60166f506cd23d8
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-19 19:44:13 -0400

    ,

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -262,7 +262,7 @@ const src_firstpass = {
     "[dɡ]ʲ" : "j",
     "([aeɛioɔu])([^aeɛioɔu])$" : "$1.$2$1", // mirror vowel when word ends with a consonant
     "([aeɛioɔu])([^aeɛioɔu])([^aeɛioɔu]+)$" : "$1$2.$3$1", // mirror vowel when word ends with a consonant
-    "(?<=i\\.ˈ)i" : "e"
+    "(?<=i\\.ˈ)i" : "e",
     "([^aeɛioɔu]*)([aeɛioɔu])\\.(ˈ?)\\1" : "$3$1$2",
 
 }
