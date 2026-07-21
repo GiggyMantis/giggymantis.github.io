e955fe3e3feb8327b48c087430230eba845723f8
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-21 16:18:38 -0400

    bweh

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -485,7 +485,7 @@ const proma_firstpass = {
     "ɔ" : "o",
     "(?<=ˈ[^\\.]*)ʊ(?=[mb])" : "o", // Latin short u -> PRi *o 
     "ʊ" : "u",
-    "ʷ(?=[eiɛ])" : "",
+    "(?<=[kɡ\\.?ˈ?)w(?=[eiɛ])" : "",
 }
 
 const proma_orthography = {
