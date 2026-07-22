273bd980f1984875c1c25f67185c10fa305bdc02
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-21 20:07:36 -0400

    test

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -517,7 +517,7 @@ const proma_firstpass = {
     "(?<=[aeiou]\\.ˈ?)l(?=[aeiou])" : "r", // rhotacism of intervocalic single l
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // degemination
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1" : ".$2$1",
-    "([^aeiou\\.ˈ])\\1" : "$1",
+    "([^aeiou\\.ˈ])(?=\\1)" : "",
     "t(\\.?ˈ?)j" : "ʦ$1ʦ", // second palatalization
     "d(\\.?ˈ?)j" : "ʣ$1ʣ",
     "s(\\.?ˈ?)j" : "ʃ$1ʃ",
@@ -533,6 +533,9 @@ const proma_firstpass = {
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // i'm lazy so... double degemination
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1" : ".$2$1",
     "([^aeiou\\.ˈ])(?=\\1)" : "",
+    "o(?=[^\\.]*\\.[^\\.][aeo])" : "wa", // o-breaking, e-breaking
+    "e(?=[^\\.]*\\.[^\\.][aeo])" : "ja",
+    "([aeiou])\\.(ˈ?)([^aeiou])([wj])" : "$1$3.$2$4",
 
 }
 
