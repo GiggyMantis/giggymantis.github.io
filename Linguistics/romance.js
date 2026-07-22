b0cc18a3112ea5462cbcebeb923cdaacf015d72d
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-07-21 22:09:29 -0400

    ?

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -517,7 +517,7 @@ const proma_firstpass = {
     "(?<=[aeiou]\\.ˈ?)l(?=[aeiou])" : "r", // rhotacism of intervocalic single l
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // degemination
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1" : ".$2$1",
-    "([^aeiou\\.ˈ])(?=\\1)" : "",
+    "(.)(?=\\1)" : "",
     "t(\\.?ˈ?)j" : "ʦ$1ʦ", // second palatalization
     "d(\\.?ˈ?)j" : "ʣ$1ʣ",
     "s(\\.?ˈ?)j" : "ʃ$1ʃ",
@@ -530,15 +530,12 @@ const proma_firstpass = {
     "l(?=\\.?ˈ?i)" : "ʎ",
     "k(?=\\.?ˈ?[ei])" : "ʧ",
     "ɡ(?=\\.?ˈ?[ei])" : "ʤ",
-    "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // i'm lazy so... double degemination
-    "([^aeiou\\.ˈ])\\.(ˈ?)\\1" : ".$2$1",
-    "([^aeiou\\.ˈ])(?=\\1)" : "",
     "o(?=[^\\.]*\\.[^\\.][aeo])" : "wa", // o-breaking, e-breaking
     "e(?=[^\\.]*\\.[^\\.][aeo])" : "ja",
     "([aeiou])\\.(ˈ?)([^aeiou])([wj])" : "$1$3.$2$4",
-    "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // i'm lazy so... triple degemination!!!!
+    "([^aeiou\\.ˈ])\\.(ˈ?)\\1(?=([^aeiou]))" : "$1.$2", // i'm lazy so... double degemination!!!!
     "([^aeiou\\.ˈ])\\.(ˈ?)\\1" : ".$2$1",
-    "([^aeiou\\.ˈ])(?=\\1)" : "",
+    "(.)(?=\\1)" : "",
 
 }
 
