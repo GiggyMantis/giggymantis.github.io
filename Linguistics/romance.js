c0f9b12d76edc25206978614b621fbf82afbb3bd
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-08-05 19:20:10 -0400

    see how i laugh at you

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -659,6 +659,7 @@ const roma_orthography = {
 const arom_firstpass = {
     "([aieouə])\\.(ˈ?)([^aieouə])e\\(a\\)" : "$1$3.$2ja",
     "e\\(a\\)" : "ja",
+    "(?<=ʎ)ə$" : "e",
     "e$" : "i", // -e -> -i
     "^(ˈ?)s(?=k)" : "$1ʃ", // sc -> shc
     "pt" : "t", // pt clusters reduced as in strimtu <- Latin *strinctum
