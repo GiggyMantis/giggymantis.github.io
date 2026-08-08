305ac2599603c01941a655fe6d2014705d78e05a
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-08-07 20:39:26 -0400

    test

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -601,7 +601,7 @@ const roma_firstpass = {
     "[ɲʎ]" : "j", // ɲ, ʎ -> j
     "[əi](?=n$)|[əi](?=\\.?n\\.?[^n\\.])|[əi](?=m\\.?[^aeiouə])" : "ɨ", // ə, i -> ɨ when before n, but not nn, or a consonant cluster starting with m
     "(?<=[aeiouəɨ]\\.?ˈ?)l(?=\\.?ˈ?[aə])" : "", // l -> ∅ / V_[aə]
-    "a\\.ə" : "a",
+    "a\\.ə" : "P",
     "pt" : "t", // pt clusters reduced as in strâmt <- Latin *strinctum
     "(?<=ˈ[^\\.]*r)i" : "ɨ", // stressed ri -> rɨ
     "[ieə](?=r\\.ˈ?[^aeiouəɨ])" : "ɨ", // i, e, ə -> ɨ / _rC ()
