2e2bfdabc21996aee21f09c9607f123c41f52f9e
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2026-09-05 15:56:06 -0400

    ^! ^!!! ^!!!

--- Linguistics/romance.js
+++ Linguistics/romance.js
@@ -1722,9 +1722,9 @@ const neap_firstpass = {
     "v(?=\\.?ˈ?v?j)" : "b", // v(v)j -> b(b)j
     "([pt])(?=\\.?ˈ?\\1?j)" : "k", // p(p)j, t(t)j -> k(k)j
     "[bɡ](?=\\.ˈ?j)" : "j", // bj, ɡj -> jj, jj
-    "$(ˈ?)j(?=j)" : "$1", // jj -> j / #_
+    "^(ˈ?)j(?=j)" : "$1", // jj -> j / #_
     "f(\\.?ˈ?)j" : "ʃ$1ʃ", // fj -> ʃʃ
-    "$(ˈ?)ʃ(?=ʃ)" : "$1", // ʃʃ -> ʃ / #_
+    "^(ˈ?)ʃ(?=ʃ)" : "$1", // ʃʃ -> ʃ / #_
     "([pt])(?=\\.?ˈ?\\1?j)" : "k", // TODO: voiced palatal combo outcomes
     "[aɛeiɔou]$" : "ə", // vowels neutralize to schwa at the ends of words
 }
